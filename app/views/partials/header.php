3a8eef5a52e85aeb0a12ec98b852e7c3ef19d91f
igorn2659 <igorn2659@gmail.com>
2026-01-09 15:41:56 +0000

    update header

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -5,13 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cloud Storage</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/styles.css">
+    <?php
+        $basePath = dirname($_SERVER['SCRIPT_NAME']);
+        if ($basePath === '/' || $basePath === '\') {
+            $basePath = '';
+        }
+    ?>
+    <link rel="stylesheet" href="<?php echo $basePath; ?>/css/styles.css">
 </head>
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="/">Cloud Storage</a>
+        <a class="navbar-brand" href="<?php echo $basePath; ?>/">Cloud Storage</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -19,22 +25,22 @@
             <ul class="navbar-nav ml-auto">
                 <?php if (App\Utils\Auth::check()): ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="/">My Files</a>
+                        <a class="nav-link" href="<?php echo $basePath; ?>/">My Files</a>
                     </li>
                     <?php if (App\Utils\Auth::hasRole('admin')): ?>
                         <li class="nav-item">
-                            <a class="nav-link" href="/admin/users">Admin</a>
+                            <a class="nav-link" href="<?php echo $basePath; ?>/admin/users">Admin</a>
                         </li>
                     <?php endif; ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="/logout">Logout</a>
+                        <a class="nav-link" href="<?php echo $basePath; ?>/logout">Logout</a>
                     </li>
                 <?php else: ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="/login">Login</a>
+                        <a class="nav-link" href="<?php echo $basePath; ?>/login">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/register">Register</a>
+                        <a class="nav-link" href="<?php echo $basePath; ?>/register">Register</a>
                     </li>
                 <?php endif; ?>
             </ul>
