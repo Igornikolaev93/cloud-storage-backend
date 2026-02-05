275f830e3522b887620d3486be76630e22c550dd
igorn2659 <igorn2659@gmail.com>
2026-02-05 11:07:13 +0000

    route

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,8 @@
 <?php
 declare(strict_types=1);
 
-// --- ГАРАНТИРОВАННАЯ ЗАГРУЗКА ROUTER ---
+// This file defines the application's routes but was missing the code to run them.
+
 require_once __DIR__ . '/utils/Router.php';
 
 use App\Controllers\AdminController;
@@ -12,17 +13,19 @@ use App\Controllers\HomeController;
 use App\Controllers\UserController;
 use App\Utils\Router;
 
-// Главная страница
+// --- Route Definitions ---
+
+// Home Page
 Router::get('/', [HomeController::class, 'index']);
 
-// Аутентификация
+// Authentication
 Router::get('/register', [AuthController::class, 'showRegisterForm']);
 Router::post('/register', [AuthController::class, 'handleRegister']);
 Router::get('/login', [AuthController::class, 'showLoginForm']);
 Router::post('/login', [AuthController::class, 'handleLogin']);
 Router::get('/logout', [AuthController::class, 'logout']);
 
-// --- Управление файлами ---
+// File Management
 Router::get('/files', [FileController::class, 'index']);
 Router::get('/files/list', [FileController::class, 'list']);
 Router::get('/files/get/{id}', [FileController::class, 'get']);
@@ -30,13 +33,21 @@ Router::post('/files/add', [FileController::class, 'add']);
 Router::post('/files/rename', [FileController::class, 'rename']);
 Router::post('/files/remove', [FileController::class, 'remove']);
 
-// --- Управление доступом к файлам ---
+// File Sharing
 Router::get('/files/share/{id}', [FileController::class, 'getSharedUsers']);
 Router::put('/files/share/{id}/{user_id}', [FileController::class, 'shareWithUser']);
 Router::delete('/files/share/{id}/{user_id}', [FileController::class, 'unshareWithUser']);
 
-// --- Управление папками ---
+// Directory Management
 Router::post('/directories/add', [DirectoryController::class, 'add']);
 Router::post('/directories/rename', [DirectoryController::class, 'rename']);
 Router::get('/directories/get/{id}', [DirectoryController::class, 'get']);
 Router::post('/directories/remove/{id}', [DirectoryController::class, 'remove']);
+
+// --- DISPATCH THE ROUTER ---
+// This was the missing piece. This code actually executes the routing logic.
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$method = $_SERVER['REQUEST_METHOD'];
+
+Router::dispatch($uri, $method);
