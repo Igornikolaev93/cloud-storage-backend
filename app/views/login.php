2dc4255a0184776d7e47a314dc971d14f2017210
igorn2659 <igorn2659@gmail.com>
2026-02-10 07:37:34 +0000

    login

--- app/views/login.php
+++ app/views/login.php
@@ -1,22 +1,22 @@
 <?php include __DIR__ . '/partials/header.php'; ?>
 
-<div class="login-container">
+<div class="auth-container">
     <h2>Login</h2>
     <?php if (isset($error)): ?>
-        <p class="error"><?= htmlspecialchars($error) ?></p>
+        <div class="alert alert-danger"><?= htmlspecialchars($error) ?></div>
     <?php endif; ?>
     <form action="/login" method="post">
         <div class="form-group">
             <label for="email">Email</label>
-            <input type="email" id="email" name="email" required>
+            <input type="email" id="email" name="email" class="form-control" required>
         </div>
         <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
+            <input type="password" id="password" name="password" class="form-control" required>
         </div>
-        <button type="submit">Login</button>
+        <button type="submit" class="btn btn-primary">Login</button>
     </form>
-    <div class="register-link">
+    <div class="auth-footer">
         <p>Don't have an account? <a href="/register">Register here</a></p>
     </div>
 </div>
