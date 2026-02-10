a9bb7652461b835efdd93514d429323d2a6ca4ea
igorn2659 <igorn2659@gmail.com>
2026-02-10 07:33:52 +0000

    nvj,vkh

--- app/views/home.php
+++ app/views/home.php
@@ -1,11 +1,13 @@
 <?php include __DIR__ . '/partials/header.php'; ?>
 
-<div class="welcome-container">
-    <h1>Welcome, <?= htmlspecialchars($user['username']) ?>!</h1>
-    <p>You have successfully logged in to your cloud storage account.</p>
-    <div class="actions">
-        <a href="/files">My Files</a>
-        <a href="/logout">Log Out</a>
+<div class="hero">
+    <div class="hero-content">
+        <h1>Welcome, <?= htmlspecialchars($user['username']) ?>!</h1>
+        <p>You have successfully logged in to your cloud storage account.</p>
+        <div class="actions">
+            <a href="/files" class="btn btn-primary">My Files</a>
+            <a href="/logout" class="btn">Log Out</a>
+        </div>
     </div>
 </div>
 
