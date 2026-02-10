4f0e35be28d0270286d1ea85157235ee1c996241
igorn2659 <igorn2659@gmail.com>
2026-02-10 10:17:48 +0000

    cklnvcxcl

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -57,15 +57,29 @@ class AuthController extends BaseController
 
             $user = User::findByEmail($email);
 
-            if (!$user || !password_verify($password, $user['password_hash'])) {
-                throw new Exception('Invalid email or password.');
+            if ($user) {
+                // User exists, verify password
+                if (password_verify($password, $user['password_hash'])) {
+                    Auth::login($user['id']);
+                    header('Location: /files');
+                    exit;
+                } else {
+                    throw new Exception('Invalid email or password.');
+                }
+            } else {
+                // User does not exist, create a new one
+                $username = explode('@', $email)[0];
+                $userId = User::create(['username' => $username, 'email' => $email, 'password' => $password]);
+
+                if (!$userId) {
+                    throw new Exception('Registration failed.');
+                }
+
+                $newUser = User::findById($userId);
+                Auth::login($newUser['id']);
+                header('Location: /files');
+                exit;
             }
-
-            Auth::login($user['id']);
-
-            header('Location: /files');
-            exit;
-
         } catch (Exception $e) {
             $this->render('login', ['error' => $e->getMessage()]);
         }
