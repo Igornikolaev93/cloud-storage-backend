b6db3d55a0205485f564ada9dc4e1379eae4883e
igorn2659 <igorn2659@gmail.com>
2026-03-10 07:40:18 +0000

    update config

--- app/config/config.php
+++ app/config/config.php
@@ -8,12 +8,12 @@ ini_set('default_charset', 'UTF-8');
 
 define('DB_CONFIG', [
     'driver'    => 'pgsql',
-    // Используем direct connection как в SQL Editor (это РАБОТАЕТ)
-    'host'      => 'db.vvqrogorxkspdudypriy.supabase.co',
+    // ВАЖНО: Используем pooler хост (поддерживает IPv4)
+    'host'      => 'aws-0-eu-central-1.pooler.supabase.com',
     'port'      => '5432',
-    'dbname'    => 'postgres',  // Сначала подключаемся к postgres
-    // В direct connection используем просто 'postgres'
-    'username'  => 'postgres',
+    'dbname'    => 'postgres',
+    // ВАЖНО: Для pooler нужно использовать username с project-ref
+    'username'  => 'postgres.vvqrogorxkspdudypriy',
     'password'  => 'Ybrjkftdbujhm16',
     'charset'   => 'utf8',
     'options'   => [
